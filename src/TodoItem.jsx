ea6c984fc18ea655790da24298ff96af0aa0ef4d
SennCode <andres.alonso.senn@gmail.com>
2023-08-04 14:50:06 +0200

    tododnd_actualización

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -4,35 +4,40 @@ import React from "react";
 
 const TodoItem = React.forwardRef(
   ({ todo, removeTodo, updateTodo, ...props }, ref) => {
-      const { id, title, completed } = todo;
+    const { id, title, completed } = todo;
 
-  return (
-    <>
-      <article {...props} ref={ref} className="flex gap-4 border-b border-b-gray-400 dark:bg-gray-800 transition-all duration-500">
-        <button
-          className={`rounded-full w-5 h-5 border-2 flex-none
+    return (
+      <>
+        <article
+          {...props}
+          ref={ref}
+          className="flex gap-4 border-b border-b-gray-400 dark:bg-gray-800"
+        >
+          <button
+            className={`rounded-full w-5 h-5 border-2 flex-none
           ${
             completed
               ? "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex justify-center items-center"
               : "inline-block"
           }`}
-          onClick={() => updateTodo(id)}
-        >
-          {completed && <IconCheck />}
-        </button>
-        <p
-          className={`text-gray-500 transition-all duration-500 dark:text-gray-300 grow ${
-            completed && "line-through"
-          }`}
-        >
-          {title}
-        </p>
-        <button className="flex-none" onClick={() => removeTodo(id)}>
-          <IconCross />
-        </button>
-      </article>
-    </>
-  );
-});
+            onClick={() => updateTodo(id)}
+          >
+            {completed && <IconCheck />}
+          </button>
+          <p
+            className={`text-gray-500 transition-all duration-500 dark:text-gray-300 grow ${
+              completed && "line-through"
+            }`}
+          >
+            {title}
+          </p>
+          <button className="flex-none" onClick={() => removeTodo(id)}>
+            <IconCross />
+          </button>
+        </article>
+      </>
+    );
+  }
+);
 
 export default TodoItem;
